4f97bdd57e210e1f196e8d410cba5ab1902dc8fd
Joshua Hernandez <99joshernandez@gmail.com>
2023-04-13 14:45:51 -0500

    initailize

--- 350.py
+++ 350.py
@@ -13,6 +13,5 @@ def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
             hashTable[i] = 1
     
     return [k for k, v in hashTable.items() if v > 1]
-    
 
 print(intersect([1,2,2,1],[2,2]))
