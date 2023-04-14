5719252adba5f68cbe7c51c8be7cd0a772663fda
Joshua Hernandez <99joshernandez@gmail.com>
2023-04-14 10:50:29 -0500

    complete q350

--- 350.py
+++ 350.py
@@ -1,18 +1,71 @@
+## O(n + m) Method using Hashmap
+# def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
+#     if len(nums2) > len(nums1):
+#         return intersect(nums2, nums1)
+#     hashTable = {}
+#     interArr = []
+#     for i in nums1:
+#         if i in hashTable.keys():
+#             hashTable[i] += 1
+#         else:
+#             hashTable[i] = 1
+    
+#     for i in nums2:
+#         if i in hashTable.keys():
+#             if hashTable[i] > 0:
+#                 interArr.append(i)
+#                 hashTable[i] -= 1
+    
+#     return interArr
+
+## O(mlogm + nlogn) Method using two pointers and Merge Sort
+def mergesort(arr):
+    if len(arr) > 1:
+
+        mid = len(arr) // 2 
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergesort(left)
+        mergesort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
 def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
-    hashTable = {}
-    for i in nums1:
-        if i in hashTable.keys():
-            hashTable[i] += 1
-        else:
-            hashTable[i] = 1
+    mergesort(nums1)
+    mergesort(nums2)
     
-    for i in nums2:
-        if i in hashTable.keys():
-            hashTable[i] += 1
+    interArr = []
+    i = j = 0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            interArr.append(nums1[i])
+            i += 1
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
         else:
-            hashTable[i] = 1
+            j += 1
     
-    return [k for k, v in hashTable.items() if v > 1]
-
-
+    return interArr
 print(intersect([1,2,2,1],[2,2]))
+print(intersect([4,9,5],[9,4,9,8,4]))
