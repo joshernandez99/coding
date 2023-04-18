2ff13f16e9ce459756acd108191405e90d649af9
Joshua Hernandez <99joshernandez@gmail.com>
2023-04-18 15:46:52 -0500

    complete 121

--- 121.py
+++ 121.py
@@ -7,9 +7,14 @@
 #             maxProfit = max(maxProfit, profit)
 #     return maxProfit
 
-
+## O(n) Method using Kanades Algorithm
 def maxProfit(prices: list[int]) -> int:
-    
+    maxProfit = 0
+    minProfit = prices[0]
+    for x in prices:
+        minProfit = min(minProfit, x)
+        maxProfit = max(maxProfit, x - minProfit)
+    return maxProfit
 
 
 print(maxProfit([7,1,5,3,6,4]))
